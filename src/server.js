af56ca70a24072c5731036481af0dc55b54c6a40
Vibilan <vibilan2793@gmail.com>
2026-07-09 18:56:42 +0530

    database connection

--- src/server.js
+++ src/server.js
@@ -1,4 +1,8 @@
 import express from "express";
+import dotenv from "dotenv";
+import pool from "./config/db.js";
+
+dotenv.config();
 
 const app = express();
 
@@ -6,6 +10,16 @@ app.use(express.json());
 
 app.get("/", (req, res) => {
     res.json({ message: "Hello world." })
+});
+
+app.get("/db-test", async (req, res) => {
+    try {
+        const result = await pool.query("SELECT NOW();");
+        res.send(result.rows[0].now);
+    }
+    catch (error) {
+        res.status(500).send(error.message);
+    }
 });
 
 app.listen(3000, () => {
